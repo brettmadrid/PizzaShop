bb117ffd18e40d611673990e7f2a43a0f7769dad
brettmadrid <brettmadrid9@gmail.com>
2020-07-26 21:18:20 -0700

    added food sections

--- sliceline/src/Data/FoodData.js
+++ sliceline/src/Data/FoodData.js
@@ -1,42 +1,50 @@
-export const foods = [
+export const foodItems = [
   {
     name: 'Cheese Pizza',
     img: '/img/pizza.png',
-    section: 'pizza',
+    section: 'Pizza',
   },
   {
     name: 'Pepperoni Pizza',
     img: '/img/pizza2.jpeg',
-    section: 'pizza',
+    section: 'Pizza',
   },
   {
     name: 'Chicken Pizza',
     img: '/img/chicken-pizza.jpeg',
-    section: 'pizza',
+    section: 'Pizza',
   },
   {
     name: 'Veggie Pizza',
     img: '/img/healthy-pizza.jpeg',
-    section: 'pizza',
+    section: 'Pizza',
   },
   {
     name: 'Burger',
     img: '/img/burger.jpeg',
-    section: 'sandwiches',
+    section: 'Sandwiches',
   },
   {
     name: 'Gyro',
     img: '/img/gyro.jpeg',
-    section: 'sandwiches',
+    section: 'Sandwiches',
   },
   {
     name: 'Shrimp PoBoy',
     img: '/img/sandwich.jpeg',
-    section: 'sandwiches',
+    section: 'Sandwiches',
   },
   {
     name: 'Fries',
     img: '/img/fries.jpeg',
-    section: 'sides',
+    section: 'Sides',
   },
 ]
+
+export const foods = foodItems.reduce((res, food) => {
+  if (!res[food.section]) {
+    res[food.section] = []
+  }
+  res[food.section].push(food)
+  return res
+}, {})
