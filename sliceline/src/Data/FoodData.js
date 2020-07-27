dc7a94ee81af25460bc56b09cb199e454fd41c5c
brettmadrid <brettmadrid9@gmail.com>
2020-07-26 21:00:43 -0700

    added section key to food data

--- sliceline/src/Data/FoodData.js
+++ sliceline/src/Data/FoodData.js
@@ -2,33 +2,41 @@ export const foods = [
   {
     name: 'Cheese Pizza',
     img: '/img/pizza.png',
+    section: 'pizza',
   },
   {
     name: 'Pepperoni Pizza',
     img: '/img/pizza2.jpeg',
+    section: 'pizza',
   },
   {
     name: 'Chicken Pizza',
     img: '/img/chicken-pizza.jpeg',
+    section: 'pizza',
   },
   {
     name: 'Veggie Pizza',
     img: '/img/healthy-pizza.jpeg',
+    section: 'pizza',
   },
   {
     name: 'Burger',
     img: '/img/burger.jpeg',
+    section: 'sandwiches',
   },
   {
     name: 'Gyro',
     img: '/img/gyro.jpeg',
+    section: 'sandwiches',
   },
   {
     name: 'Shrimp PoBoy',
     img: '/img/sandwich.jpeg',
+    section: 'sandwiches',
   },
   {
     name: 'Fries',
     img: '/img/fries.jpeg',
+    section: 'sides',
   },
 ]
