14f77b06971a7476a0da3e5feb799c08217948d2
brettmadrid <brettmadrid9@gmail.com>
2020-08-03 21:03:43 -0700

    added prices to foodData

--- sliceline/src/Data/FoodData.js
+++ sliceline/src/Data/FoodData.js
@@ -3,41 +3,49 @@ export const foodItems = [
     name: 'Cheese Pizza',
     img: '/img/pizza.png',
     section: 'Pizza',
+    price: 10,
   },
   {
     name: 'Pepperoni Pizza',
     img: '/img/pizza2.jpeg',
     section: 'Pizza',
+    price: 11,
   },
   {
     name: 'Chicken Pizza',
     img: '/img/chicken-pizza.jpeg',
     section: 'Pizza',
+    price: 12,
   },
   {
     name: 'Veggie Pizza',
     img: '/img/healthy-pizza.jpeg',
     section: 'Pizza',
+    price: 12,
   },
   {
     name: 'Burger',
     img: '/img/burger.jpeg',
     section: 'Sandwiches',
+    price: 5,
   },
   {
     name: 'Gyro',
     img: '/img/gyro.jpeg',
     section: 'Sandwiches',
+    price: 5,
   },
   {
     name: 'Shrimp PoBoy',
     img: '/img/sandwich.jpeg',
     section: 'Sandwiches',
+    price: 6,
   },
   {
     name: 'Fries',
     img: '/img/fries.jpeg',
     section: 'Sides',
+    price: 2,
   },
 ]
 
