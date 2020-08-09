e464e43a58b18fb6ecb0f0da359328a58f9e1843
brettmadrid <brettmadrid9@gmail.com>
2020-08-08 17:57:28 -0700

    Edit and Delete

--- sliceline/src/FoodDialog/FoodDialog.js
+++ sliceline/src/FoodDialog/FoodDialog.js
@@ -98,6 +98,7 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
   const quantity = useQuantity(openFood && openFood.quantity)
   const toppings = useToppings(openFood.toppings)
   const choiceRadio = useChoice(openFood.choice)
+  const isEditing = openFood.index > -1
 
   function close() {
     setOpenFood()
@@ -108,6 +109,13 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
     quantity: quantity.value,
     toppings: toppings.toppings,
     choice: choiceRadio.value,
+  }
+
+  function editOrder() {
+    const newOrders = [...orders]
+    newOrders[openFood.index] = order
+    setOrders(newOrders)
+    close()
   }
 
   const addToOrder = () => {
@@ -136,10 +144,11 @@ function FoodDialogContainer({ openFood, setOpenFood, setOrders, orders }) {
         </DialogContent>
         <DialogFooter>
           <ConfirmButton
-            onClick={addToOrder}
+            onClick={isEditing ? editOrder : addToOrder}
             disabled={openFood.choices && !choiceRadio.value}
           >
-            Add to order: {formatPrice(getPrice(order))}
+            {isEditing ? `Update order: ` : `Add to order: `}
+            {formatPrice(getPrice(order))}
           </ConfirmButton>
         </DialogFooter>
       </Dialog>
